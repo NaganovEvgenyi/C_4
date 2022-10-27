89ffb6cc1f3ed84efdd0da0293c698c52c670ae7
NaganovEvgenyi <enaganov@inbox.ru>
2022-10-27 20:59:45 +0300

    3 задача

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@
      }
      return step;
  }
- */
+ 
 
 // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
@@ -46,3 +46,19 @@
      return sum;
  }
 
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]*/
+
+int[] array = GetNumber(8);
+Console.WriteLine($"[{String.Join(".", array)}]");
+
+int[] GetNumber(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(9999);
+    }
+    return result;
+}
